35add2f6d3ccc34e95a4344358e06a964a454039
Hung Pham <phamphihungbk@gmail.com>
2021-06-11 22:31:55 +0800

    update

--- web/tests/TestCase.php
+++ web/tests/TestCase.php
@@ -2,26 +2,11 @@
 
 namespace Tests;
 
-use App\Exceptions\Handler;
-use Exception;
-use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Notification;
-use Illuminate\Support\Facades\Queue;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, RefreshDatabase;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Queue::fake();
-        Notification::fake();
-
-        Artisan::call('config:clear');
-    }
+    use CreatesApplication, RefreshDatabase, DatabaseMigrations;
 }
