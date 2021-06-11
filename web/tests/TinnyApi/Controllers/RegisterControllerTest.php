6d819ba949bfc781e1925ace755b8c7b1fa4b9df
Hung Pham <phamphihungbk@gmail.com>
2021-06-11 15:22:51 +0800

    update test case for register controller

--- web/tests/TinnyApi/Controllers/RegisterControllerTest.php
+++ web/tests/TinnyApi/Controllers/RegisterControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\TinnyApi\Controllers;
 
 use Illuminate\Http\Response;
 use Tests\TestCase;
+use TinnyApi\Models\UserModel;
 
 class RegisterControllerTest extends TestCase
 {
@@ -20,5 +21,69 @@ class RegisterControllerTest extends TestCase
         ])
             ->assertStatus(Response::HTTP_CREATED)
             ->assertSee('We sent a confirmation email to test@test.com');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecausePasswordIsTooShort()
+    {
+        $this->postJson(route('api.auth.register'), [
+            'name'                  => 'test',
+            'email'                 => 'test@test.com',
+            'password'              => 'secret',
+            'password_confirmation' => 'secret',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('The password must be at least 8 characters');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecausePasswordIsWeak()
+    {
+        $this->postJson(route('api.auth.register'), [
+            'name'                  => 'test',
+            'email'                 => 'test@test.com',
+            'password'              => 'secretxxx',
+            'password_confirmation' => 'secretxxx',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('This password is just too common. Please try another!');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecausePasswordsNotMatch()
+    {
+        $this->postJson(route('api.auth.register'), [
+            'name'                  => 'test',
+            'email'                 => 'test@test.com',
+            'password'              => 'secretxxx1',
+            'password_confirmation' => 'secretxxx2',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('The password confirmation does not match');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecauseEmailAlreadyRegistered()
+    {
+        UserModel::factory()->create([
+            'email'    => 'test@test.com',
+            'password' => bcrypt('secretxxx'),
+        ]);
+
+        $this->postJson(route('api.auth.register'), [
+            'email'                 => 'test@test.com',
+            'password'              => 'secretxxx-test',
+            'password_confirmation' => 'secretxxx-test',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('email has already been taken');
     }
 }
