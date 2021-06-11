2a511f1538798e032dac22229cb99fe108b4b123
HUNG PHAM <phamphihungbk@gmail.com>
2021-06-12 00:09:56 +0800

    Update Testcase for register controller (#7)
    * update test case for register controller
    
    * update test case and travis config
    
    * update
    
    * update
    
    * update
    
    * update travis config
    
    * update travis config
    
    * update travis config

--- web/src/Factories/UserModelFactory.php
+++ web/src/Factories/UserModelFactory.php
@@ -19,11 +19,14 @@ class UserModelFactory extends Factory
     public function definition()
     {
         return [
+            'id' => 'id',
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
+            'facebook' => 'facebook.com',
+            'twitter' => 'twitter.com',
         ];
     }
 }
--- web/tests/TinnyApi/Controllers/RegisterControllerTest.php
+++ web/tests/TinnyApi/Controllers/RegisterControllerTest.php
@@ -4,15 +4,18 @@ namespace Tests\TinnyApi\Controllers;
 
 use Illuminate\Http\Response;
 use Tests\TestCase;
+use TinnyApi\Models\UserModel;
 
 class RegisterControllerTest extends TestCase
 {
+    const REGISTER_ROUTER = 'api.auth.register';
+
     /**
      * @test
      */
     public function testCanRegister()
     {
-        $this->postJson(route('api.auth.register'), [
+        $this->postJson(route(self::REGISTER_ROUTER), [
             'name' => 'test',
             'email' => 'test@test.com',
             'password' => 'secretxxx-test',
@@ -20,5 +23,69 @@ class RegisterControllerTest extends TestCase
         ])
             ->assertStatus(Response::HTTP_CREATED)
             ->assertSee('We sent a confirmation email to test@test.com');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecausePasswordIsTooShort()
+    {
+        $this->postJson(route(self::REGISTER_ROUTER), [
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('The password must be at least 8 characters');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecausePasswordIsWeak()
+    {
+        $this->postJson(route(self::REGISTER_ROUTER), [
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'secretxxx',
+            'password_confirmation' => 'secretxxx',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('This password is just too common. Please try another!');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecausePasswordsNotMatch()
+    {
+        $this->postJson(route(self::REGISTER_ROUTER), [
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'secretxxx1',
+            'password_confirmation' => 'secretxxx2',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('The password confirmation does not match');
+    }
+
+    /**
+     * @test
+     */
+    public function testCannotRegisterBecauseEmailAlreadyRegistered()
+    {
+        UserModel::factory()->create([
+            'email' => 'test@test.com',
+            'password' => bcrypt('secretxxx'),
+        ]);
+
+        $this->postJson(route(self::REGISTER_ROUTER), [
+            'email' => 'test@test.com',
+            'password' => 'secretxxx-test',
+            'password_confirmation' => 'secretxxx-test',
+        ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSee('email has already been taken');
     }
 }
