cc7876614d54180b6a0e152fc59bb4662ac4b61c
HUNG PHAM <phamphihungbk@gmail.com>
2021-07-24 12:27:25 +0800

    complete refactor (#22)

--- web/config/database.php
+++ web/config/database.php
@@ -65,14 +65,21 @@ return [
 
         'mysql_testing' => [
             'driver' => 'mysql',
-            'host' => env('APP_NAME') === 'testing' ? env('DB_HOST') : 'mysql',
-            'database' =>  env('APP_NAME') === 'testing' ? env('DB_DATABASE') : 'db_testing',
-            'username' => env('APP_NAME') === 'testing' ? env('DB_USERNAME') : 'api-user',
-            'password' => env('APP_NAME') === 'testing' ? env('DB_PASSWORD') : 'api-password' ,
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'host' => env('APP_NAME') === 'testing' ? env('DB_HOST', '127.0.0.1') : 'mysql',
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('APP_NAME') === 'testing' ? env('DB_DATABASE', 'forge') : 'db_testing',
+            'username' => env('APP_NAME') === 'testing' ? env('DB_USERNAME', 'forge') : 'api-user',
+            'password' => env('APP_NAME') === 'testing' ? env('DB_PASSWORD', '') : 'api-password',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
@@ -135,7 +142,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
